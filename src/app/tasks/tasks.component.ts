276fa748e81af23624443edfbef609d52ef9860d
tstevanovich <tonys@yahoo.com>
2025-02-20 15:55:44 -0500

    style webkit scrollbar, add task component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,11 +1,12 @@
 import { Component, input } from '@angular/core';
 
 import { User } from '../models';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss'
 })
