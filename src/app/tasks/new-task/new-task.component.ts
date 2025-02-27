1b5216f3b8efa60f1fee288297399667e3fad000
tstevanovich <tonys@yahoo.com>
2025-02-27 11:28:19 -0500

    two way binding task

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,13 +1,17 @@
-import { Component, output } from '@angular/core';
+import { Component, model, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.scss'
 })
 export class NewTaskComponent {
   cancelAddTask = output<void>();
+  enteredTitle = model<string>('');
+  enteredSummary = model<string>('');
+  enteredDate = model<string>('');
 
   onCancel() {
     this.cancelAddTask.emit();
